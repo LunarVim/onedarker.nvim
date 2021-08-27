376c8d264f0e24751ee51158d8524b207fda7b79
Christian Chiarulli <chris.machine@pm.me>
2021-08-26 23:52:04 -0400

    fg = fg

--- lua/lualine/themes/onedarker.lua
+++ lua/lualine/themes/onedarker.lua
@@ -23,10 +23,10 @@ return {
     b = {fg = colors.fg, bg = colors.bg},
     c = {fg = colors.fg, bg = colors.bg}
   },
-  insert = {a = {fg = colors.bg, bg = colors.green, gui = 'bold'}},
-  visual = {a = {fg = colors.bg, bg = colors.purple, gui = 'bold'}},
-  command = {a = {fg = colors.bg, bg = colors.yellow, gui = 'bold'}},
-  replace = {a = {fg = colors.bg, bg = colors.red1, gui = 'bold'}},
+  insert = {a = {fg = colors.fg, bg = colors.green, gui = 'bold'}},
+  visual = {a = {fg = colors.fg, bg = colors.purple, gui = 'bold'}},
+  command = {a = {fg = colors.fg, bg = colors.yellow, gui = 'bold'}},
+  replace = {a = {fg = colors.fg, bg = colors.red1, gui = 'bold'}},
   inactive = {
     a = {fg = colors.gray1, bg = colors.bg, gui = 'bold'},
     b = {fg = colors.gray1, bg = colors.bg},
