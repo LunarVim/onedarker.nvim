b7889529df7decdaf4869bf17b0bcead4d8426ef
Christian Chiarulli <chris.machine@pm.me>
2022-07-04 22:57:06 -0400

    update onedarker

--- lua/onedarker/init.lua
+++ lua/onedarker/init.lua
@@ -1,16 +1,26 @@
-local M = {}
-
-function M.setup()
-  local highlights = require "onedarker.highlights"
-  vim.cmd "hi clear"
-  if vim.fn.exists "syntax_on" then
-    vim.cmd "syntax reset"
-  end
-  vim.o.background = "dark"
-  vim.o.termguicolors = true
-  vim.g.colors_name = "onedarker"
-  highlights.setup()
-  vim.cmd [[colorscheme onedarker]]
+vim.api.nvim_command("hi clear")
+if vim.fn.exists("syntax_on") then
+    vim.api.nvim_command("syntax reset")
 end
+vim.o.background = "dark"
+vim.o.termguicolors = true
+vim.g.colors_name = "onedarker"
 
-return M
+local util = require("onedarker.util")
+Config = require("onedarker.config")
+C = require("onedarker.palette")
+local highlights = require("onedarker.highlights")
+local Treesitter = require("onedarker.Treesitter")
+local markdown = require("onedarker.markdown")
+local Whichkey = require("onedarker.Whichkey")
+local Git = require("onedarker.Git")
+local LSP = require("onedarker.LSP")
+
+
+local skeletons = {
+    highlights, Treesitter, markdown, Whichkey, Git, LSP
+}
+
+for _, skeleton in ipairs(skeletons) do
+    util.initialise(skeleton)
+end
